1f417a1a71b8bc4f419f9b98a80f9321bd92cd04
Prathamesh <100895075+PrathameshLohar@users.noreply.github.com>
2024-06-22 14:37:07 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def to_upper(s: str):
+    return s.upper()
+
+def to_lower(s: str):
+    return s.lower()
+
+if __name__ == "__main__":
+    name = "Prathamesh"
+    print (to_upper(name))
+    print (to_lower(name))
+
